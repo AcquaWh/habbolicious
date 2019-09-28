ee9e32a2bf1c11fae671a4270da334aad7a80c1a
AcquaWh <acquaentertainment@gmail.com>
2019-09-28 03:08:59 -0700

    Administrador

---
+++ resources/views/admin/terminos/edit.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+@section('title','HabboLicious • Panel de administración')
+
+@section('customstyles')
+@endsection
+
+@section('content')
+@endsection
+
+@section('customscripts')
+@endsection
