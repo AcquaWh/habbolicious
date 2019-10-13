0c0be7fbc16b6103412a49ee6145978df0151684
AcquaWh <acquaentertainment@gmail.com>
2019-10-13 00:03:27 -0700

    Login

--- resources/views/equipo.blade.php
+++ resources/views/equipo.blade.php
@@ -1,10 +1,28 @@
 @extends('layouts.habbolicious')
-@section('title','Equipo • ¡Endulza tu Habbo vida!')
+@section('title','Error • ¡Endulza tu Habbo vida!')
 
 @section('customstyles')
 @endsection
 
 @section('content')
+<div class="contenido-inicio">
+     <div class="container">
+          <div class="row justify-content-center">
+               <div class="col-lg-12 mt-3">
+                    <div class="card error">
+                         <div class="card-body row">
+                              <div class="col-lg-12 text-center">
+                                   <img class="img-fluid" src="/img/extra/register9.gif" alt=""/>
+                              </div>
+                              <div class="col-lg-12">
+                                   <h1 class="errortitulo mt-3 text-center">La sección a la que intentas acceder no existe</h1>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </div>
+</div>
 @endsection
 
 @section('customscripts')
