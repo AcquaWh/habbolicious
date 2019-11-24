4fb7331a542f087514c699a90fb66f9c26948ee8
AcquaWh <acquaentertainment@gmail.com>
2019-11-24 13:24:57 -0700

    agregar sweets

--- app/Http/Controllers/NoticiasComentarioController.php
+++ app/Http/Controllers/NoticiasComentarioController.php
@@ -7,6 +7,7 @@ use App\Perfil;
 use App\Noticias;
 use Auth;
 use App\User;
+use App\Sweets;
 use Carbon\Carbon;
 use App\ComentariosNoticias;
 
@@ -23,6 +24,9 @@ class NoticiasComentarioController extends Controller
         $comentarios->id_noticias = $id;
         $comentarios->cuerpo = $request->input('comentario');
         if($comentarios->save()){
+            $sweets = Sweets::where('id_user',$usuario)->first();
+            $sweets->cantidad = $sweets->cantidad+30;
+            $sweets->save();
             return redirect()->route('noticias.show',$id)->with('exito','Se agrego comentario');
         }
         return redirect()->route('noticias.show',$id)->with('error','No se pudo guardar comentario');
