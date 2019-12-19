f27956d6d72876b9a25c2afb2939af7256026459
AcquaWh <acquaentertainment@gmail.com>
2019-12-19 11:16:06 -0700

    Eventos y blogs controller

--- resources/views/eventos.blade.php
+++ resources/views/eventos.blade.php
@@ -7,17 +7,15 @@
 @section('content')
 <div class="contenido-inicio">
      <div class="container">
-          <div class="row justify-content-center">
-               <div class="col-lg-12 mt-3">
-                    <div class="card error">
-                         <div class="card-body row">
-                              <div class="col-lg-12 text-center">
-                                   <img class="img-fluid" src="/img/extra/register9.gif" alt=""/>
-                              </div>
-                              <div class="col-lg-12">
-                                   <h1 class="errortitulo mt-3 text-center">La sección a la que intentas acceder no existe</h1>
-                              </div>
-                         </div>
+          <div class="row">
+               <div class="col-lg-8">
+               </div>
+               <div class="col-lg-4">
+                    <div class="contenedor-titulo" style="background-image: url(/img/extra/lpromo_cland_bundle.png);">
+                         <h4>Twitter</h4>
+                    </div>
+                    <div class="contenedor-twitter">
+                         <a class="twitter-timeline" data-height="530" href="https://twitter.com/HabboLiciousES?ref_src=twsrc%5Etfw">Twitter por Habbolicious</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                     </div>
                </div>
           </div>
